8de0ada3b0b3da4a58e98a409bc62258c0c0c4fc
ssmith180 <scs@exitsinc.com>
2019-04-30 13:28:35 -0500

    Don't do that again

--- handler.js
+++ handler.js
@@ -1,17 +1,23 @@
 'use strict';
 
-const Couchbase = require('couchbase'),
-  util = require('util'),
-  UUID = require('uuid'),
-  Joi = require('joi'),
-  SearchQuery = Couchbase.SearchQuery,
+const Couchbase = require('couchbase');
+const util = require('util');
+const uuid = require('uuid');
+const Joi = require('joi');
+const $q = require('bluebird');
+const _ = require('lodash');
+const SearchQuery = Couchbase.SearchQuery,
   SearchFacet = Couchbase.SearchFacet,
   N1qlQuery = Couchbase.N1qlQuery,
   MatchQuery = Couchbase.MatchQuery;
+const denialAgencies = require('./denialagencies');
 
 var cluster = new Couchbase.Cluster('couchbase://100.24.37.195');
 cluster.authenticate('the_gauth', '4y8xs#7Cnk');
 var bucket = cluster.openBucket('dp');
+
+bucket.queryPromise = $q.promisify(bucket.query);
+bucket.upsertPromise = $q.promisify(bucket.upsert);
 
 bucket.on('error', error => {
   console.dir(error);
@@ -228,159 +234,225 @@ module.exports.delete = (event, context, callback) => {
   });
 };
 
-module.exports.fulltextsandbox = (event, context, callback) => {
+module.exports.dpaddresssearch = (event, context, callback) => {
+
   context.callbackWaitsForEmptyEventLoop = false;
-  var data = JSON.parse(event.body);
-  var response = {};
+  var response = {}, data = JSON.parse(event.body);
 
+  // two-part search like name search, searching over the address fields; create a separate address index for this.
+  var addressAndCity = [data.address, data.city].join(' ').toUpperCase().replace(/[^A-Z0-9 ]*/g, '');
 
-  // Basic query structure; specify a type of query,
-  // then call this bucket.query business.
+  var addressQuery = SearchQuery.new('entity_address', SearchQuery.match(addressAndCity));
+  addressQuery.fields('country');
 
-  // So I guess we just try... 
-  var query = SearchQuery.new("entity_name_fulltext", SearchQuery.match("mohammed"));
+  var resultSets = [], queries = [addressQuery];
+  var country = data.country && data.country.toUpperCase();
 
-  bucket.query(query, (error, result) => {
-    if (error) {
+  console.log(data);
+  console.log(data.country);
+  console.log(country);
+
+  var resultPacker = function(result) {
+    resultSets.push(result);
+    if (resultSets.length == queries.length) {
+      response = {
+        statusCode: 200,
+        body: JSON.stringify(resultSets)
+      };
+      callback(null, response);
+    }
+  };
+
+  var errorHandler = function(error) {
+    if (!response.statusCode) {
       response = {
         statusCode: 500,
         body: JSON.stringify({
           code: error.code,
           message: error.message
-        })
+        })   
       };
       return callback(null, response);
     }
-    response = {
-      statusCode: 200,
-      body: JSON.stringify(result)
+  };
+
+  bucket.queryPromise(addressQuery)
+    .then(function(results) {
+      if (country) _.remove(results, function(result) { return result.fields.country !== country && result.fields.country !== 'XX'; });
+      return $q.map(results, function(record) {
+        return bucket.queryPromise(N1qlQuery.fromString('select name, address_1, address_2, city, state, country, start_Date, last_update, denial_Code, denial_references from dp where META().id = \'' + record.id + '\''))
+          .then(function(singleResult) {
+            return {
+              score: record.score,
+              name: singleResult[0].name,
+              denial_agency: denialAgencies[singleResult[0].denial_Code],
+              address_1: singleResult[0].address_1,
+              address_2: singleResult[0].address_2,
+              city: singleResult[0].city,
+              state: singleResult[0].state,
+              country: singleResult[0].country,
+              start_date: singleResult[0].start_Date,
+              last_update: singleResult[0].last_Update,
+              denial_references: singleResult[0].denial_references
+            };
+          });
+      });
+    })
+    .then(function(results) {
+      var id = 'exitsinc:dp:lambda_test:search:' + _.replace(uuid(), /-/g, '');
+      data.type = 'addresssearch';
+      return bucket.upsertPromise(id, data).then(function() { return results; });
+    })
+    .then(resultPacker)
+    .catch(errorHandler);
+
+};
+
+
+var doNameSearch = function(firstName, lastName, companyName, country, reverseOnly) {
+
+  var isSimilarQuery, names;
+  if (firstName || lastName) {
+    firstName = (firstName || '').toUpperCase().replace(/[^A-Z0-9 ]*/g, '');
+    lastName = (lastName || '').toUpperCase().replace(/[^A-Z0-9 ]*/g, '');
+    var for_name = firstName + ' ' + lastName;
+    var rev_name = lastName + ', ' + firstName;
+    names = [for_name, rev_name];
+
+    isSimilarQuery = SearchQuery.new('entity_name_fulltext', SearchQuery.match(for_name));
+    isSimilarQuery.fields("name", "country", "start_Date", "last_Update", "denial_Code", "denial_references");
+  } else {
+    companyName = (companyName || ' ').toUpperCase().replace(/[^A-Z0-9 ]*/g, '');
+    names = [companyName];
+    isSimilarQuery = SearchQuery.new('entity_name_fulltext', SearchQuery.match(companyName));
+    isSimilarQuery.fields("name", "country", "start_Date", "last_Update", "denial_Code", "denial_references");
+  }
+
+  return bucket.queryPromise(isSimilarQuery)
+  .then(function(results) {
+    if (country) _.remove(results, function(result) { return result.fields.country !== country && result.fields.country !== 'XX'; });
+    return $q.map(results, function(record) {
+      return bucket.queryPromise(N1qlQuery.fromString('select name, address_1, address_2, city, state, country, start_Date, last_update, denial_Code, denial_references, includeReverse from dp where META().id = \'' + record.id + '\''))
+        .then(function(singleResult) {
+          return {
+            score: record.score,
+            name: singleResult[0].name,
+            denial_agency: denialAgencies[singleResult[0].denial_Code],
+            address_1: singleResult[0].address_1,
+            address_2: singleResult[0].address_2,
+            city: singleResult[0].city,
+            state: singleResult[0].state,
+            country: singleResult[0].country,
+            start_date: singleResult[0].start_Date,
+            last_update: singleResult[0].last_Update,
+            denial_references: singleResult[0].denial_references,
+            includeReverse: singleResult[0].includeReverse
+          };
+        });
+    });
+  })
+  .then(function(results) {
+    if (reverseOnly) console.log(results.length.toString() + ' results before reverse filtering');
+    results = _.filter(results, function(result) { return !reverseOnly || result.includeReverse; });
+    if (reverseOnly) console.log(results.length.toString() + ' results after reverse filtering');
+    // Does this need to be a promise?
+    var isMatches = _.map(_.remove(results, function(result) { return _.some(names, function(name) { return result.name === name; });}), function(item) { return _.omit(item, ['score']);});
+    var containsMatches = _.map(_.remove(results, function(result) { return _.some(names, function(name) { _.includes(result.name, name); }); }), function(item) { return _.omit(item, ['score'])});
+    var similarMatches = results;
+    return {
+      matchCount: isMatches.length + containsMatches.length + similarMatches.length,
+      is: isMatches,
+      contains: containsMatches,
+      similar: similarMatches
     };
-    callback(null, response);
   });
 
 };
+
+
+module.exports.reversesearch = (event, context, callback) => {
+  // Load past name searches
+  context.callbackWaitsForEmptyEventLoop = false;
+
+  var pastNameSearchesN1ql = 'select firstName, lastName, companyName from dp where type = \'namesearch\' group by firstName, lastName, companyName';
+  bucket.queryPromise(N1qlQuery.fromString(pastNameSearchesN1ql))
+    .then(function(pastSearches) {
+    // Got the past searches, re-execute them on reverseSearch=true entities only.
+    // This may require re-inserting reverse search entities into their own type.
+    
+    var reverseSearchResults = [], searchCount = 0;
+    console.log('past search count = ' + pastSearches.length.toString());
+
+    var responseHandler = function() {
+      console.log('checking response... ' + searchCount.toString() + ' to ' + pastSearches.length.toString());
+      if (searchCount === pastSearches.length) {
+        // done getting results, send it all back?
+        var response = {
+          statusCode: 200,
+          body: JSON.stringify( { count: reverseSearchResults.length, hits: reverseSearchResults } )
+        };
+        console.log('executing callback');
+        return callback(null, response);
+      }
+    };
+
+    _.map(pastSearches, function(pastSearch) {
+
+      doNameSearch(pastSearch.firstName, pastSearch.lastName, pastSearch.companyName, null, true)
+      .then(function(results) {
+        searchCount++;
+        console.log('Search ' + searchCount.toString() + ' complete: ' + JSON.stringify(results));
+        if (results.matchCount) {
+          console.log('Reverse search hits found');
+          pastSearch.reverseSearchResults = results;
+          reverseSearchResults.push(pastSearch);
+        }
+        return searchCount;
+      })
+      .then(responseHandler);
+    });
+
+  })
+
+};
+
 
 module.exports.dpnamesearch = (event, context, callback) => {
 
   context.callbackWaitsForEmptyEventLoop = false;
   var response = {};
 
-  // This is a bit of a disaster right now, as it's
-  // vulnerable to SQL injection. Needs to be better-patterned.
-  // Also, the CONTAINS query is brutally slow. Might be able
-  // to save that with full-text search.
-
-// select entity_Id, name, country, start_Date, last_Update, denial_Code, denial_references from dp where name like '%MOHAMMED';
-  var queryRoot = ' entity_Id, name, country, start_Date, last_Update, denial_Code, denial_references from dp ';
-
   var data = JSON.parse(event.body);
 
-  var matchQueryHandler = function(error, result) {
-    if (error) {
-      if (!response.statusCode) {
-        response = {
-          statusCode: 500,
-          body: JSON.stringify({
-            code: error.code,
-            message: error.message
-          })   
-        };
-        return callback(null, response);
-      }
-    } else {
-      resultSets.push(result);
-      if (resultSets.length == queries.length) {
-        response = {
-          statusCode: 200,
-          body: JSON.stringify(resultSets)
-        };
-        callback(null, response);
-      }
-    }
-  };
-
-  var ftsQueryHandler = function(error, result) {
-    if (error) {
-      if (!response.statusCode) {
-        response = {
-          statusCode: 500,
-          body: JSON.stringify({
-            code: error.code,
-            message: error.message
-          })   
-        };
-        return callback(null, response);
-      }
-    } else {
-      var details = 'select \'contains\', ' + queryRoot + 'where META().id in [';
-      var idList = [];
-      result.forEach(function(item) { idList.push('\'' + item.id + '\'');});
-      details += idList.join(',') + ']';
-      var detailQuery = N1qlQuery.fromString(details);
-      bucket.query(detailQuery, matchQueryHandler);
-    }
-  };
-
-  var queries = [];
-  var resultSets = [];
-
-  if (data.firstName || data.lastName) {
-    var fsafe = data.firstName.replace('\'', '\\\'');
-    var lsafe = data.lastName.replace('\'', '\\\'');
-    var for_name = fsafe + ' ' + lsafe;
-    var rev_name = lsafe + ', ' + fsafe;
-
-    var isForwardQuery = N1qlQuery.fromString('select \'is\', ' + queryRoot + 'where name = \'' + for_name + '\'');
-    var isReverseQuery = N1qlQuery.fromString('select \'is\', ' + queryRoot + 'where name = \'' + rev_name + '\'');
-
-    var containsForwardQuery = SearchQuery.new('entity_name_fulltext', SearchQuery.match(for_name));
-    var containsReverseQuery = SearchQuery.new('entity_name_fulltext', SearchQuery.match(rev_name));
-
-    containsForwardQuery.fields("entity_Id", "name", "country", "start_Date", "last_Update", "denial_Code", "denial_references");
-    containsReverseQuery.fields("entity_Id", "name", "country", "start_Date", "last_Update", "denial_Code", "denial_references");
-
-    queries = [isForwardQuery, isReverseQuery, containsForwardQuery, containsReverseQuery];
-
-    bucket.query(isForwardQuery, matchQueryHandler);
-    bucket.query(isReverseQuery, matchQueryHandler);
-    bucket.query(containsForwardQuery, ftsQueryHandler);
-    bucket.query(containsReverseQuery, ftsQueryHandler);
-
-  } else {
-    var nsafe = data.name.replace('\'', '\\\'');
-
-    var isQuery = N1qlQuery.fromString('select \'is\', ' + queryRoot + 'where name = \'' + nsafe + '\'');
-    var containsQuery = SearchQuery.new('entity_name_fulltext', SearchQuery.match(nsafe));
-
-    queries = [isQuery, containsQuery];
-  }
-
-
-};
-
-module.exports.dpaddresssearch = (event, context, callback) => {
-  // use N1QL to query for all documents in Couchbase and return them.
-
-  // change how the function waits to respond.
-  context.callbackWaitsForEmptyEventLoop = false;
-  var response = {};
-  var statement = util.format("SELECT META().id, %s.* FROM %s limit %i offset %i", bucket._name, bucket._name, event.queryStringParameters.limit, event.queryStringParameters.offset);
-  var query = Couchbase.N1qlQuery.fromString(statement);
-  bucket.query(query, (error, result) => {
-    if (error) {
-      response = {
-        statusCode: 500,
-        body: JSON.stringify({
-          code: error.code,
-          message: error.message
-        })
-      };
-      return callback(null, response);
-    }
+  doNameSearch(data.firstName, data.lastName, data.companyName, data.country)
+  .then(function(results) {
+    var id = 'exitsinc:dp:lambda_test:search:' + _.replace(uuid(), /-/g, '');
+    data.type = 'namesearch';
+    data.firstName = data.firstName && data.firstName.toUpperCase();
+    data.lastName = data.lastName && data.lastName.toUpperCase();
+    data.companyName = data.companyName && data.companyName.toUpperCase();
+    data.country = data.country && data.country.toUpperCase();
+    data.results = results;
+    return bucket.upsertPromise(id, data).then(function() { return results; });
+  })
+  .then(function(result) {
     response = {
       statusCode: 200,
       body: JSON.stringify(result)
     };
     callback(null, response);
+  })
+  .catch(function(error) {
+    if (!response.statusCode) {
+      response = {
+        statusCode: 500,
+        body: JSON.stringify({
+          code: error.code,
+          message: error.message
+        })   
+      };
+      return callback(null, response);
+    }
   });
+
 };
